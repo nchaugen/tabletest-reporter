236398b2cc7dfe1a2cb036b708bb367db11fcb1d
Nils Christian Haugen <nch@scienta.no>
2025-11-27 16:36:54 +0100

    Support pipe character in table values

---
+++ reporter/src/test/java/io/github/nchaugen/tabletest/reporter/TableValuesWithPipeTest.java
@@ -0,0 +1,58 @@
+package io.github.nchaugen.tabletest.reporter;
+
+import org.junit.jupiter.api.Test;
+
+import static io.github.nchaugen.tabletest.reporter.ReportFormat.ASCIIDOC;
+import static io.github.nchaugen.tabletest.reporter.ReportFormat.MARKDOWN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TableValuesWithPipeTest {
+
+    private static final String TABLE_CONTEXT_YAML = """
+        title: Escaped pipes
+        columnCount: 3
+        headers:
+          - value: "a"
+          - value: "b"
+          - value: "a|b"
+        rows:
+          - - value: "|"
+            - value: '|'
+            - value: "Text with | character"
+        """;
+
+    @Test
+    void supported_in_asciidoc() {
+        assertThat(ASCIIDOC.renderTable(Context.fromYaml(TABLE_CONTEXT_YAML)))
+            .isEqualTo("""
+                == ++Escaped pipes++
+                
+                [%header,cols="1,1,1"]
+                |===
+                |++a++
+                |++b++
+                |++a\\|b++
+                
+                a|++\\|++
+                a|++\\|++
+                a|++Text with \\| character++
+
+                |===
+                """
+            );
+    }
+
+    @Test
+    void supported_in_markdown() {
+        assertThat(MARKDOWN.renderTable(Context.fromYaml(TABLE_CONTEXT_YAML)))
+            .isEqualTo("""
+                ## Escaped pipes
+                
+                | a | b | a\\|b |
+                | --- | --- | --- |
+                | \\| | \\| | Text with \\| character |
+                """
+            );
+    }
+
+}
