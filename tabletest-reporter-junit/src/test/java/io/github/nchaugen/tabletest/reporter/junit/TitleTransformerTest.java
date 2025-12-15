d20b0baaab963d69383779bcb564941cf8fc8969
Nils Christian Haugen <nch@scienta.no>
2025-12-15 19:35:05 +0100

    chore: update beads and test files

--- tabletest-reporter-junit/src/test/java/io/github/nchaugen/tabletest/reporter/junit/TitleTransformerTest.java
+++ tabletest-reporter-junit/src/test/java/io/github/nchaugen/tabletest/reporter/junit/TitleTransformerTest.java
@@ -33,14 +33,14 @@ class TitleTransformerTest {
     }
 
     @TableTest("""
-        Method Name With Parameters                                    | Expected Title?
-        testMethod(java.lang.String)                                   | Test Method
-        validateInput(java.lang.String, java.lang.Integer)             | Validate Input
-        processData(java.util.List, java.util.Map)                     | Process Data
-        handleComplexTypes(java.util.List<java.lang.String>)           | Handle Complex Types
+        Method Name With Parameters                                   | Expected Title?
+        testMethod(java.lang.String)                                  | Test Method
+        validateInput(java.lang.String, java.lang.Integer)            | Validate Input
+        processData(java.util.List, java.util.Map)                    | Process Data
+        handleComplexTypes(java.util.List<java.lang.String>)          | Handle Complex Types
         multipleGenericParams(java.util.Map<K, V>, java.util.List<T>) | Multiple Generic Params
-        simpleTest()                                                   | Simple Test
-        parseHTMLDocument(java.lang.String, boolean)                   | Parse HTML Document
+        simpleTest()                                                  | Simple Test
+        parseHTMLDocument(java.lang.String, boolean)                  | Parse HTML Document
         """)
     void shouldStripParameterTypesFromMethodNames(String input, String expectedTitle) {
         String methodName = input.contains("(") ? input.substring(0, input.indexOf('(')) : input;
